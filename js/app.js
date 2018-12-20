2fac3ac75dbf4569d9f15906c5c2802b6ad2d3e8
Janish Khanna <janish.khanna233@gmail.com>
2018-12-20 10:42:14 -0600

    Installed my Service Worker

---
+++ js/app.js
@@ -0,0 +1,9 @@
+if(navigator.serviceWorker) {
+    navigator.serviceWorker.register('../sw.js')
+    .then(function() {
+        console.log('Service Worker Installed');
+    })
+    .catch(function() {
+        console.log('Service Worker Did Not Installed');
+    })
+}
